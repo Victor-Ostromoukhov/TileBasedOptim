69ad60cc9ec14f0368ca664c1ed90f5d11abce58
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-12 14:24:45 +0200

    vo

---
+++ README.md
@@ -0,0 +1,2 @@
+# TileBasedOptim
+# TileBasedOptim
---
+++ TileBasedOptim/TileBasedOptim.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 12 Apr 2022 *)
+
+BeginPackage["TileBasedOptim`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
