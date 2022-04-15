f259934e6b58440534ce83a72ee7c5ad669826b4
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-15 16:52:49 +0200

    vo

--- TileBasedOptim/TileBasedOptim.m
+++ TileBasedOptim/TileBasedOptim.m
@@ -1,8 +1,8 @@
-(* fibo-hilbert.m
-   V.O. version 2002/12/14
+(* TileBasedOptim.m
+   2022-04 vo, based on fibo-hilbert.m (version 2002/12/14)
 *)
  
-(****************** params *******************)
+(****************** globals *******************)
 SetOptions[Graphics, ImageSize -> { 1024, Automatic}];
 
 mf := MatrixForm
@@ -10,17 +10,18 @@ T :=  Transpose
 PI = Pi//N;
 known := ValueQ
 i2s[n_,len_:6] := ToString[NumberForm[n, len-1, NumberPadding -> "0"]]
-r2s[n_,len_:6,dec_:5] := ToString[NumberForm[n, {len,dec}]]
+r2s[n_,len_:6,dec_:5] := ToString[NumberForm[n, {len,dec}, NumberPadding -> "0"]]
 tab2s[tab_] := StringJoin @ Table[" "<>ToString[tab[[i]]]<>" ",{i,Length[tab]}]
 tab2snosep[tab_] := StringJoin @ Table[ ToString[tab[[i]]] ,{i,Length[tab]}]
 tab2ssep[tab_] := StringJoin @ Table["_"<>ToString[tab[[i]]],{i,Length[tab]}]
 tab2ssepComma[tab_] := StringJoin[ToString[tab[[1]]], Table[","<>ToString[tab[[i]]],{i,Min[2,Length[tab]], Length[tab]}] ]
-digits2str[digits_] := StringJoin[ToString /@ digits] 
 str2n[str_] := FromDigits[#, 2] & @ Table[StringTake[str, {i}] // ToExpression, {i, StringLength[str]}]
 
-(*------------------------- new code 2022-04 vo, based on fibo-hilbert.m (version 2002/12/14) -------------------------*)
+pid := "_pid"<>ToString[$ProcessID]<>"_kid"<>ToString[$KernelID]
+execPrefix = "~/bin/";
+
 (*------------------------- constants -------------------------*)
-phi = N[GoldenRatio,32];
+phi = N[GoldenRatio,16];
 oneoverphi = 1/phi;
 oneoverphi2 = oneoverphi^2;
 
@@ -50,9 +51,21 @@ FIBOFxy[symbols_] := FIBOF/@symbols
 FIBOphitab = Table[phi^-i, {i, 32}] // N;
 FIBOPhi[s_] := Sum[FIBOphitab[[i]] s[[i]], {i, Length[s]}] 
 
+getGeneralizedL2discrepancy[pts_, dbg_:False] :=
+    Module[ {execString,nDims = Length[First@pts],prog,returnCode, discrepancy},
+    	If[ !FileExistsQ["tmp/"], CreateDirectory["tmp/"] ];
+    	prog = "getGeneralizedL2Discrepancy_from_file";
+        Export["tmp/tmp"<>pid<>".dat",N[pts]];
+        execString =  prog<>" -i tmp/tmp"<>pid<>".dat -o tmp/res"<>pid<>".dat -d "<>ToString[nDims]<>" > /dev/null";
+        returnCode = Run[execPrefix<>execString];
+        If[dbg, Print[execString -> returnCode ] ];
+        discrepancy = Import["tmp/res"<>pid<>".dat"][[1,2]];
+        Run["rm tmp/tmp"<>pid<>".dat tmp/res"<>pid<>".dat"];
+        discrepancy
+   ] (* getGeneralizedL2discrepancy *)
 
 demoFiboSFC[niters_:15] :=
-    Module[ {},
+    Module[ {dbg},
     	dbg = False;
 		tlst = {{type1,{0,0}, {{1,0},{0,1}}, {0,0}, {}} };
 		Graphics[ getFiboSFCTilesGL[tlst] ]//Print;
@@ -64,11 +77,11 @@ demoFiboSFC[niters_:15] :=
 		,{iter,niters}];
 
 		Graphics[ getFiboSFCTilesGL[tlst,showValue] ]//Print;
-]
+	] (* demoFiboSFC *)
 
 
 subdivFiboSFCTiles[tlst_] :=
-    Block[ {res={}, tileType,refPt,v1,v2,samplingPt,fcode },
+    Module[ {res={}, tileType,refPt,v1,v2,samplingPt,fcode },
     	Table[
 			{tileType,refPt,{v1,v2},samplingPt,fcode} = tlst[[ind]];
             Switch[tileType
@@ -114,8 +127,8 @@ subdivFiboSFCTiles[tlst_] :=
     ] (* subdivFiboSFCTiles *)
 
 getsfcFiboSFC[tlst_] :=
-    Block[ {sfc={}, tileType,refPt,v1,v2,samplingPt,fcode,delta=7},
-    	Table[
+    Module[ {sfc={}, tileType,refPt,v1,v2,samplingPt,fcode,delta=7},
+    	Do[
 			{tileType,refPt,{v1,v2},samplingPt,fcode} = tlst[[ind]];
    			AppendTo[sfc,refPt + (v1/Norm[v1]+v2/Norm[v2])/1/phi^((Length[fcode]+delta)/2) ];
             Switch[tileType
@@ -132,18 +145,17 @@ getsfcFiboSFC[tlst_] :=
 
 
 getFiboSFCTilesGL[tlst_,params_:showSFC] :=
-    Block[ {gl={},tileType,refPt,v1,v2,samplingPt,fcode,cont,
+    Module[ {gl={},tileType,refPt,v1,v2,samplingPt,fcode,cont,sfc,
     		bortedStyle={Cyan,AbsoluteThickness[1]}, sfcStyle={Orange,AbsoluteThickness[3]}},
-    	Table[
+    	Do[
 			{tileType,refPt,{v1,v2},samplingPt,fcode} = tlst[[ind]];
 			cont = {refPt,refPt+v1,refPt+v1+v2,refPt+v2,refPt};
     		If[BitAnd[params,showValue] > 0, AppendTo[gl,{GrayLevel[FIBOPhi[Reverse@fcode]],Polygon@cont}] ];
-			AppendTo[gl,Flatten[#,1]& @ {bortedStyle,Line[cont] } ];
+			AppendTo[gl,Flatten[#,1]& @ {bortedStyle,Line@cont } ];
 			If[BitAnd[params,showTileType] > 0, AppendTo[gl, {Text[Style[tileType,Bold,14,Blue],refPt+(v1+v2)/2,{-1,-1}]} ] ];		
 			If[BitAnd[params,showTilefcode] > 0, AppendTo[gl, {Text[Style[tab2snosep@fcode,Bold,14,Gray],refPt+(v1+v2)/2,{0,1}]} ] ];
     	,{ind,Length[tlst]}];
-    	sfc = getsfcFiboSFC[tlst];
-    	If[BitAnd[params,showSFC] > 0, AppendTo[gl,Flatten[#,1]& @ {sfcStyle,Line@sfc}] ];
+    	If[BitAnd[params,showSFC] > 0, sfc = getsfcFiboSFC[tlst]; AppendTo[gl,Flatten[#,1]& @ {sfcStyle,Line@sfc}] ];
     	Return[gl]
     ] (* getFiboSFCTilesGL *)
     
