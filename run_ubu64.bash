120da786b7b265529c8fb21dfc43651b2f173813
Victor Ostromoukhov <Victor.Ostromoukhov@gmail.com>
2022-04-29 00:27:06 +0200

    vo

--- run_ubu64.bash
+++ run_ubu64.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nthreads=64
+nthreads=8
 
 for (( level=200; level<=729; level++ )) do
     echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 1024 
