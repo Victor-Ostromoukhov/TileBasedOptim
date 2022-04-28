584d6a299232220606d828d995a49ed0bfd277a0
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-28 19:28:17 +0200

    vo

--- run_ubu64.bash
+++ run_ubu64.bash
@@ -2,9 +2,9 @@
 
 nthreads=64
 
-for (( level=3; level<=729; level++ )) do
-        echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 10000 > traces/tr_${level}.txt
-        ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 10000 >> traces/tr_${level}.txt
+for (( level=2; level<=729; level++ )) do
+        echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 100000 > traces/tr_${level}.txt
+        ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 100000 >> traces/tr_${level}.txt
 done
 
 
