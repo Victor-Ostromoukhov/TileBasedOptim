b72cdd15905e1879957623296f88c5e31fb3a006
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-29 10:14:28 +0200

    vo

--- run_M1.bash
+++ run_M1.bash
@@ -2,8 +2,8 @@
 
 nthreads=8
 
-#ntrials=4096 0r 16384 or 65536
-ntrials=4096
+#ntrials=1024 4096 0r 16384 or 65536
+ntrials=1024
 
 for (( level=180; level<=199; level++ )) do
     echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials}
