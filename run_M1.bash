6780189cc1c791cb2caada3a9b10a768cab2146a
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-29 10:11:36 +0200

    vo

---
+++ run_M1.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+nthreads=8
+
+#ntrials=4096 0r 16384 or 65536
+ntrials=4096
+
+for (( level=180; level<=199; level++ )) do
+    echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials}
+    ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials} >> traces/tr_${level}.txt
+done
+
+
