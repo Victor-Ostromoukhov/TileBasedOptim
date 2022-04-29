468fd96eaf2cd1343db1b58ac3d23ad26cb07608
ostrom <victor.ostromoukhov@liris.cnrs.fr>
2022-04-29 21:04:23 +0200

    vo

--- run_ubu64.bash
+++ run_ubu64.bash
@@ -3,7 +3,7 @@
 nthreads=64
 
 #ntrials=1024 or 4096 or 16384 or 65536
-ntrials=4096
+ntrials=16384
 
 for (( level=2; level<=729; level++ )) do
     echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials}
