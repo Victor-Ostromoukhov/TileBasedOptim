ff44d88d5c397feb57fbb5246e2b86be8aba1e87
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-04-29 19:08:34 +0200

    vo

--- oneJob.bash
+++ oneJob.bash
@@ -3,5 +3,8 @@
 nthreads=$1
 level=$2
 
-echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat > traces/tr_${level}.txt
-~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat >> traces/tr_${level}.txt
+#ntrials=4096 0r 16384 or 65536
+ntrials=65536
+
+echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials} > traces/tr_${level}.txt
+~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n ${ntrials} >> traces/tr_${level}.txt
