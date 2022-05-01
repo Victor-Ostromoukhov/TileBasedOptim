5c299adf2f2d439480ce5e3254d7c2ace7555b3b
Victor Ostromoukhov <Victor.Ostromoukhov@gmail.com>
2022-05-01 12:55:30 +0200

    vo

--- runJobs.bash
+++ runJobs.bash
@@ -2,7 +2,7 @@
 
 nthreads=32
 
-for (( level=2; level<=729; level++ )) do
+# for (( level=700; level<=729; level++ )) do
    for (( trial=1; trial<=1; trial++ )) do
         sbatch -n ${nthreads} -J L${level} oneJob.bash ${nthreads} ${level}
     done
