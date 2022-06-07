4031fef2bd0ba631132f58abb462bbfe7bea0d89
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-07 11:44:47 +0200

    vo

--- src/Optimize_L2Discrepancy_2DTiles_Noise_Cancelling/ubu64Version/runOneRepetition.sh
+++ src/Optimize_L2Discrepancy_2DTiles_Noise_Cancelling/ubu64Version/runOneRepetition.sh
@@ -23,5 +23,5 @@ mkdir -p Repetition_${repetition}/Traces
 
 for (( level=2 ; level <= 242 ; level++ ))
 	do
-		~/bin/Optimize_L2Discrepancy_For_2D_Tiles -t ${nbthreads} -n 1024 -i ../Optimize_L2Discrepancy_For_2D_Tiles/Data/Optim_Input/2D_0m2net_set_1_level_${level}.dat -o Repetition_${repetition}/Output/2D_0m2net_set_1_level_${level}_Opt.dat >> Repetition_${repetition}/Traces/Trace_Level_${level}.dat
+		~/bin/OptimTiles2D -t ${nbthreads} -n 1024 -i ../Optimize_L2Discrepancy_For_2D_Tiles/Data/Optim_Input/2D_0m2net_set_1_level_${level}.dat -o Repetition_${repetition}/Output/2D_0m2net_set_1_level_${level}_Opt.dat >> Repetition_${repetition}/Traces/Trace_Level_${level}.dat
 	done
