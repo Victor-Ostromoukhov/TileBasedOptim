1e9edcdf027f3ab9ae99e5f74a34316cc66d3796
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-09 21:44:08 +0200

    vo

--- src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition.sh
+++ src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition.sh
@@ -22,8 +22,9 @@ mkdir -p ../Repetitions_Heaviside/Repetition_${repetition}/Output
 mkdir -p ../Repetitions_Heaviside/Repetition_${repetition}/Traces
 
 nIterations=1024
-nItegrandsPerIteration=16384
-for (( level=2 ; level <= 729 ; level++ ))
+#nItegrandsPerIteration=16384
+nItegrandsPerIteration=4096
+for (( level=47 ; level <= 729 ; level++ ))
 	do
         echo ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i ../Optimize_MSE_2DTiles/Data/Optim_Data_Input/Tiles/2D_0m2net_set_1_level_${level}.dat -o ../Repetitions_Heaviside/Repetition_${repetition}/Output/level_${level}.dat --nbPoints ${level} --integrandType 5 -g $nItegrandsPerIteration
         ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i ../Optimize_MSE_2DTiles/Data/Optim_Data_Input/Tiles/2D_0m2net_set_1_level_${level}.dat -o ../Repetitions_Heaviside/Repetition_${repetition}/Output/level_${level}.dat --nbPoints ${level} --integrandType 5 -g $nItegrandsPerIteration | tee ../Repetitions_Heaviside/Repetition_${repetition}/Traces/Trace_Level_${level}.dat
