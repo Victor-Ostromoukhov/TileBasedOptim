dec43dc7bf8ae57848b8b9652d549351f484de3e
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-09 21:44:47 +0200

    vo

--- src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition.sh
+++ src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition.sh
@@ -23,7 +23,8 @@ mkdir -p ../Repetitions_Heaviside/Repetition_${repetition}/Traces
 
 nIterations=1024
 #nItegrandsPerIteration=16384
-nItegrandsPerIteration=4096
+#nItegrandsPerIteration=4096
+nItegrandsPerIteration=1024
 for (( level=47 ; level <= 729 ; level++ ))
 	do
         echo ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i ../Optimize_MSE_2DTiles/Data/Optim_Data_Input/Tiles/2D_0m2net_set_1_level_${level}.dat -o ../Repetitions_Heaviside/Repetition_${repetition}/Output/level_${level}.dat --nbPoints ${level} --integrandType 5 -g $nItegrandsPerIteration
