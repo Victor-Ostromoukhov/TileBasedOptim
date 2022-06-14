4d83b1131f3d9b6dd6b7688befbcd8b25323808d
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-14 11:42:07 +0200

    vo

--- src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition_SoftEllipses.sh
+++ src/Optimize_MSE_2DTiles/ubu64Version/runOneRepetition_SoftEllipses.sh
@@ -25,8 +25,8 @@ nIterations=1024
 
 #nItegrandsPerIteration=4096
 #nItegrandsPerIteration=16384
-nItegrandsPerIteration=65536
-#nItegrandsPerIteration=262144
+#nItegrandsPerIteration=65536
+nItegrandsPerIteration=262144
 #nItegrandsPerIteration=524288
 lst=(3 4 5 6 7 8 9 10 11 13 15 17 19 21 24 27 31 34 39 44 50 56 63 72 81 92 103 117 132 149 168 190 215 243 275 310 350 396 447 505 571 645 729)
 for level in ${lst[@]}
