c95f7f1fd62310bac2337cc3985e97702f44d2f0
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-20 13:55:27 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/in2p3/OneRepetitionOneLevel_SoftEllipses.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/in2p3/OneRepetitionOneLevel_SoftEllipses.sh
@@ -39,4 +39,6 @@ else
 fi
 
 
+echo ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i $infname -o $outfname --outputNextStep  --nbPoints $to --integrandType 2 -g $nItegrandsPerIteration --limit $limit > "${TracesDir}Trace_Level_${to}.dat"
+
 ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i $infname -o $outfname --outputNextStep  --nbPoints $to --integrandType 2 -g $nItegrandsPerIteration --limit $limit > "${TracesDir}Trace_Level_${to}.dat"
