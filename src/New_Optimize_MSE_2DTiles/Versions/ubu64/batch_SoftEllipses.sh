8117c2cb88fb3d58ac51bb7c4353fa8995eaf3f4
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-06-21 16:17:27 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/ubu64/batch_SoftEllipses.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/ubu64/batch_SoftEllipses.sh
@@ -10,5 +10,5 @@ lst_length=1
 for (( ind=1 ; ind <= lst_length ; ind++ ))
 	do
         fname=${inputFiles[$((${ind}))]}
-        ./runOneRepetitionLauncher_SoftEllipses.sh $ind $fname
+        echo ./runOneRepetitionLauncher_SoftEllipses.sh $ind $fname
 	done
