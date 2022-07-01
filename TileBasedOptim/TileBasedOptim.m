8c0079bc7f5bea4e1ce0729e4f77bfd3e986eb58
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-07-01 12:27:13 +0200

    vo

--- TileBasedOptim/TileBasedOptim.m
+++ TileBasedOptim/TileBasedOptim.m
@@ -3100,7 +3100,6 @@ prepHeavisideND[innDims_:2, setNo_:1] :=
     Module[ {nIntegrands,nDims,suffix,maxtime,dir,precision,maxRecursion,batchsz,nbatches,res1024,res,trial,finalLength,resfname,alldata,hppfname,integral,muDiscotinuity,normVector,alpha,j,
     	integrandTypeLabel,hppsuffix,cppsuffix,varName},
     	nIntegrands = 512 1024;
-    	nIntegrands =  1024;
         If[$ProcessorCount != 10 && Length[Kernels[]] < $ProcessorCount*2, LaunchKernels[$ProcessorCount*2] ];
         nDims = innDims;
 
