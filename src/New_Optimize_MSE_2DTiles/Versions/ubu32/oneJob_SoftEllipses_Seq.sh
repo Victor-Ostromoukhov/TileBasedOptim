8cd38a09408a8dec7eaf1feb7fdc397727d4d489
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-01 21:16:32 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/ubu32/oneJob_SoftEllipses_Seq.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/ubu32/oneJob_SoftEllipses_Seq.sh
@@ -15,28 +15,21 @@ nItegrandsPerIteration=65536
 
 
 InputDir="../../Data/Input/Tiles_Seq_PrevLevel/"
-#InputDir="../../Data/Input/Tiles_Seq/"
-NextIterDir="../../Data/Output/"
 OutputDir="../../Data/Output/"
 TracesDir="../../Data/Traces"
 mkdir -p ${NextIterDir}
 mkdir -p ${OutputDir}
 mkdir -p ${TracesDir}
-  
-#first run for 3 pts
-from=2
-to=3
-limit=1
+ 
     infname=${InputDir}${fname}
     outfname=${OutputDir}${fname}
-    outputNextStep=${NextIterDir}${fname}
-
-~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i $infname -o $outfname --outputNextStep $outputNextStep --nbPoints $to --integrandType 2 -g $nItegrandsPerIteration --limit $limit
+cp ${infname} ${outfname}
 
 #the rest of counters...
 #lst=(3 4 5 6 7 8 9 10 11 13 15 17 19 21 24 27 31 34 39 44 50 56 63 72 81 92 103 117 132 149 168 190 215 243 275 310 350 396 447 505 571 645 729)
-lst=(3    4    6    9    13    19    27    39    56    81    117    168    243    350    505    729)
+#lst=(3    4    6    9    13    19    27    39    56    81    117    168    243    350    505    729)
 #lst=(3    9    27    81    243    729)
+lst=(81    243    729)
 
 lst_length=${#lst[@]}
 integrandType=2 # SoftEllipses
@@ -48,8 +41,7 @@ do
     limit=$((${lst[$((${level} - 1 ))]} + 1))
     infname=${NextIterDir}${fname}
     outfname=${OutputDir}${fname}
-    outputNextStep=${NextIterDir}${fname}
-    ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i $infname -o $outfname --outputNextStep $outputNextStep --nbPoints $to --integrandType 2 -g $nItegrandsPerIteration --limit $limit
+     ~/bin/Optimize_MSE_2DTiles -t ${nbthreads} -n $nIterations -i $infname -o $outfname --outputNextStep $outfname --nbPoints $to --integrandType ${integrandType} -g $nItegrandsPerIteration --limit $limit
 done
 
 
