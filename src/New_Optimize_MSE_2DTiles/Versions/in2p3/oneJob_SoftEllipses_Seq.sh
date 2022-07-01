b8b4a3538ed862ac235df92e254b345f9910c57e
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-07-01 15:34:28 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/in2p3/oneJob_SoftEllipses_Seq.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/in2p3/oneJob_SoftEllipses_Seq.sh
@@ -18,7 +18,7 @@ nItegrandsPerIteration=16384
 #nItegrandsPerIteration=262144
 
 
-InputDir="../../Data/Input/Tiles_Seq/"
+InputDir="../../Data/Input/Tiles_Seq_PrevLevel/"
 NextIterDir="../../Data/Output/"
 OutputDir="../../Data/Output/"
 TracesDir="../../Traces"
