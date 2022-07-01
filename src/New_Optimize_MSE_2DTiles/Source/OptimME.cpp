c122ed8370b7d9549cca6a504a50a8c26f570701
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-07-01 13:14:38 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Source/OptimME.cpp
+++ src/New_Optimize_MSE_2DTiles/Source/OptimME.cpp
@@ -19,15 +19,9 @@
 
 #define INTEGRAND_TYPE_HEAVISIDE 1
 #define INTEGRAND_TYPE_SOFTELLIPSES 2
-#define INTEGRAND_TYPE_HARDELLIPSES 3
-#define INTEGRAND_TYPE_SOFTRECTANGLES 4
-#define INTEGRAND_TYPE_HARDRECTANGLES 5
 
-#define INTEGRAND_TYPE_HEAVISIDE_NUMBER 524288
+#define INTEGRAND_TYPE_HEAVISIDE_NUMBER 262144
 #define INTEGRAND_TYPE_SOFTELLIPSES_NUMBER 131072
-#define INTEGRAND_TYPE_HARDELLIPSES_NUMBER 16384
-#define INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER 16384
-#define INTEGRAND_TYPE_HARDRECTANGLES_NUMBER 16384
 
 // Declaration des variables globales
 
@@ -41,30 +35,16 @@ struct t_GaussianStruct2D {
     double mxCInv[2 * 2];
 };
 
-struct t_SoftRectanglesStruct2D{
-  double integral;
-  double mu[2];
-  double sigma[2];
-};
-
 struct t_Heaviside2D {
   double integral;
   double muDiscotinuity[2];
   double normal[2];
 };
 
-struct t_RectanglesStruct2D{
-  double integral;
-  double mu[2];
-  double sigma[2];
-};
 
 // Déclaration des tableaux d'intégrandes
 extern t_Heaviside2D tab_Heaviside2D[INTEGRAND_TYPE_HEAVISIDE_NUMBER];
 extern t_GaussianStruct2D tab_SoftEllipses2D[INTEGRAND_TYPE_SOFTELLIPSES_NUMBER];
-extern t_GaussianStruct2D tab_Ellipses2D[INTEGRAND_TYPE_HARDELLIPSES_NUMBER];
-extern t_SoftRectanglesStruct2D tab_SoftRectangles2D[INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER];
-extern t_RectanglesStruct2D tab_Rectangles2D[INTEGRAND_TYPE_HARDRECTANGLES_NUMBER];
 
 /* ----------- Déclaration des structures et leur "méthodes" ----------- */
 
@@ -200,42 +180,6 @@ void initializeGaussianVectors(std::vector<MatXDynamic>* sigma,std::vector<VecXD
               }
           }
           (*anal)[z-(offset*gaussianSubSetSize)] = tab_SoftEllipses2D[z].integral; // Valeur analytique
-        }
-    break;
-    case 3: // HardEllipses
-
-      for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
-        for (int i = 0; i < dimension; ++i) {
-            (*shift)[z][i] = tab_Ellipses2D[z].mu[i];  // Déplacement
-        }
-        for (int j = 0; j < dimension; ++j) {
-            for (int i = 0; i < dimension; ++i) {
-                (*sigma)[z](i, j) = tab_Ellipses2D[z].mxCInv[i + j * dimension]; // Matrice SR
-            }
-        }
-        (*anal)[z] = tab_Ellipses2D[z].integral; // Valeur analytique
-      }
-    break;
-    case 4: // SoftRectangles
-        for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
-          for (int i = 0; i < dimension; ++i) {
-              (*shift)[z][i] = tab_SoftRectangles2D[z].mu[i];  // Déplacement
-          }
-          for (int j = 0; j < dimension; ++j) {
-              (*sigma)[z](0, j) = tab_SoftRectangles2D[z].sigma[j]; // Matrice SR
-          }
-          (*anal)[z] = tab_SoftRectangles2D[z].integral; // Valeur analytique
-        }
-    break;
-    case 5: // HardRectangles
-        for (int z = 0 + offset*gaussianSubSetSize ; z < (offset + 1)*gaussianSubSetSize; z++) {
-          for (int i = 0; i < dimension; ++i) {
-              (*shift)[z][i] = tab_Rectangles2D[z].mu[i];  // Déplacement
-          }
-          for (int j = 0; j < dimension; ++j) {
-              (*sigma)[z](0, j) = tab_Rectangles2D[z].sigma[j]; // Matrice SR
-          }
-          (*anal)[z] = tab_Rectangles2D[z].integral; // Valeur analytique
         }
     break;
     default:
@@ -425,7 +369,7 @@ int main(int argc, char const *argv[]) {
       app.add_option("-o,--output",outputString,"Path to output file, default: "+outputString);
       app.add_option("-g",gaussianSubSetSize,"Number of (gaussian) integrands to iterate over in one iteration, default: "+std::to_string(gaussianSubSetSize));
       app.add_option("-m,--writeMSE",outputStringMSE,"If precised, will write the MSE of the pointset by appending it to this file, default: "+outputStringMSE);
-      app.add_option("--integrandType",integrandType,"Type of the integrand to compute MSE : 1|-> HeaviSide, 2|-> SoftEllipses, 3|-> HardEllipses, 4|-> SoftRectangles, 5|-> HardRectangles. Default: "+std::to_string(integrandType));
+      app.add_option("--integrandType",integrandType,"Type of the integrand to compute MSE : 1 -> HeaviSide, 2 -> SoftEllipses Default: "+std::to_string(integrandType));
       app.add_option("-l,--limit",limit,"The limit number behind which we don't modify the points. If left by default, will take every point. Default: "+std::to_string(limit));
       app.add_option("--outputNextStep",outputStringNextStep,"Path to output file fot next step, i.e. with all the 729 points. If left by default, it will not write it. default: "+outputStringNextStep);
       app.add_option("--writingInterval",intervalToWrite,"Defines at which interval should the pointset be written. Default: "+std::to_string(intervalToWrite));
@@ -439,15 +383,6 @@ int main(int argc, char const *argv[]) {
           break;
         case INTEGRAND_TYPE_SOFTELLIPSES:
           total_N_integrands = INTEGRAND_TYPE_SOFTELLIPSES_NUMBER;
-          break;
-        case INTEGRAND_TYPE_HARDELLIPSES:
-          total_N_integrands = INTEGRAND_TYPE_HARDELLIPSES_NUMBER;
-          break;
-        case INTEGRAND_TYPE_SOFTRECTANGLES:
-          total_N_integrands = INTEGRAND_TYPE_SOFTRECTANGLES_NUMBER;
-          break;
-        case INTEGRAND_TYPE_HARDRECTANGLES:
-          total_N_integrands = INTEGRAND_TYPE_HARDRECTANGLES_NUMBER;
           break;
         default:
         std::cerr << "L'integrande specifiee n'est pas disponible." << '\n';
