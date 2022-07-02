775fb8605a4055965adf09ddc89fc4568e5128d0
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-02 21:56:28 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Source/OptimME.cpp
+++ src/New_Optimize_MSE_2DTiles/Source/OptimME.cpp
@@ -356,7 +356,7 @@ int main(int argc, char const *argv[]) {
   std::string outputStringMSE ="MSE.dat";
   int limit = 1;
   std::vector<std::string>* restOfTheDocument = new std::vector<std::string>;
-  int intervalToWrite = 100;
+  int intervalToWrite = 25;
   /* ----------- Fin Initialisation des variables ----------- */
 
   // =========== Début CLI11 Configuration =========== //
