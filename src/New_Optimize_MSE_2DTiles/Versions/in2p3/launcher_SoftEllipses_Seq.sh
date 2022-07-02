a060e0b9c2b7818317c16f58c5441eb260d7a611
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-02 21:50:55 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/in2p3/launcher_SoftEllipses_Seq.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/in2p3/launcher_SoftEllipses_Seq.sh
@@ -8,11 +8,9 @@ fi
 
 ind=$1
 fname=$2
-
-nIterations=16
-nItegrandsPerIteration=65536
+suffix=$3
 
 nthreads=32
 
-sbatch -n ${nthreads} -J R${ind} -o "../../Traces/slurm-%j-${fname}.out" oneJob_SoftEllipses_Seq.sh ${ind} ${nthreads} ${fname}
+sbatch -n ${nthreads} -J R${ind} -o "../../Traces/slurm-%j-${fname}.out" oneJob_SoftEllipses_Seq.sh ${ind} ${nthreads} ${fname}  ${suffix}
     
