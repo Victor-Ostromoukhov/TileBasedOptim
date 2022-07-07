b564fc2d747eea2f97a9bf2d73505c546b9df693
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-07 16:27:11 +0200

    vo

--- src/New_Optimize_MSE_2DTiles/Versions/ubu32/oneJob_SoftEllipses_Pointsets.sh
+++ src/New_Optimize_MSE_2DTiles/Versions/ubu32/oneJob_SoftEllipses_Pointsets.sh
@@ -30,8 +30,9 @@ integrandType=2 # SoftEllipses
 for (( ind=0 ; ind < lst_length ; ind++ ))
 do
     npts=${lst[$((${ind}+1))]}
-    infname=${InputDir}${inputFiles[$((${ind}+1))]}
-    outfname=${OutputDir}${inputFiles[$((${ind}+1))]}
+    infname=${InputDir}${inputFiles[$((${ind}))]}
+    outfname=${OutputDir}${inputFiles[$((${ind}))]}
+    echo ~/bin/Optimize_MSE_2DTiles --nbPoints ${npts} -t ${nbthreads} -n $nIterations -i $infname -o $outfname --integrandType ${integrandType} -g $nItegrandsPerIteration
     ~/bin/Optimize_MSE_2DTiles --nbPoints ${npts} -t ${nbthreads} -n $nIterations -i $infname -o $outfname --integrandType ${integrandType} -g $nItegrandsPerIteration
 done
 
