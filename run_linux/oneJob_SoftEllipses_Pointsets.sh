2fd4998f5f1b0a8fb11c8aa622861e269cf9920c
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-10 13:56:46 +0200

    vo

--- run_linux/oneJob_SoftEllipses_Pointsets.sh
+++ run_linux/oneJob_SoftEllipses_Pointsets.sh
@@ -39,12 +39,8 @@ do
     if [ ${npts} -gt 729 ] ;
     then
         nIterations=$((${nIterations} * 2 ))
+        echo ========= ${nIterations}
     fi
-echo ${npts}
-echo ${fname}
-echo ${infname}
-echo ${outfname}
-echo ${nIterations}
     echo ~/bin/Optimize_MSE_2DTiles --nbPoints ${npts} -t ${nbthreads} -n $nIterations -i $infname -o $outfname --integrandType ${integrandType} -g $nItegrandsPerIteration  >> ${TracesDir}/t_${fname}.txt
     echo ~/bin/Optimize_MSE_2DTiles --nbPoints ${npts} -t ${nbthreads} -n $nIterations -i $infname -o $outfname --integrandType ${integrandType} -g $nItegrandsPerIteration  >> ${TracesDir}/t_${fname}.txt
 done
