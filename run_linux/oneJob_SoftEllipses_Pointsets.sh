580f4d0bc36584c8ab054395ce5e6c6296d4cb3f
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-10 13:51:41 +0200

    vo

--- run_linux/oneJob_SoftEllipses_Pointsets.sh
+++ run_linux/oneJob_SoftEllipses_Pointsets.sh
@@ -30,7 +30,7 @@ inputFiles=(`ls ${InputDir}`)
 lst_length=${#lst[@]}
 integrandType=2 # SoftEllipses
 
-echo ${lst_length}
+echo lst_length = ${lst_length}
 for (( ind=0 ; ind < lst_length ; ind++ ))
 do
     npts=${lst[$((${ind}+1))]}
