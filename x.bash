8168077890a509dbb88c0f70ae7c1fc86169380d
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-11 05:35:19 +0200

    vo

--- x.bash
+++ x.bash
@@ -19,3 +19,6 @@ echo " " >> tmp/proc
 echo "prepSoftEllipses2D[9,9]" >> tmp/proc
 chmod 700 tmp/proc
 $mexec < tmp/proc > tmp/trace_prepSoftEllipses2D.txt &
+
+echo prepSoftEllipses2D[9,9] has been launched
+echo watch tmp/trace_prepSoftEllipses2D.txt
