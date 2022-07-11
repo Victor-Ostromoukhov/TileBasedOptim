c896db02b9715317f887228a53f72919671409f4
Victor-Ostromoukhov <victor.ostromoukhov@liris.cnrs.fr>
2022-07-11 05:24:19 +0200

    vo

---
+++ x.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nthreads=8
+
+for (( level=500; level<=729; level++ )) do
+        echo ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 1024
+        ~/bin/OptimTiles2D -t ${nthreads} -i optim_data/2D_0m2net_set_1_level_${level}.dat -o optim_output/2D_0m2net_set_1_level_${level}.dat -n 1024 >> traces/tr_${level}.txt
+done
+
+
