5fadae176cbebe7ae9b926bc85c296b235a90ab4
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-07-18 16:09:28 +0200

    vo

---
+++ run_in2p3_VarSize/go_SoftEllipses_Pointsets.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+suffix="VarSize"
+
+#if [ $# -lt 1 ] ;
+#then
+#    echo "Usage : go_SoftEllipses_Pointsets <set_from> <set_to> <nthreads> [<continueFlag>]"
+#    exit
+#fi
+#
+#set_from=$1
+#set_to=$2
+#nthreads=$3
+#if [ $# -ge 4 ]; then
+#    continueFlag=$4
+#else
+#    continueFlag=false
+#fi
+#
+
+nthreads=32
+continueFlag=false
+
+inputDirs=(`ls ../Tiles_Pointsets_${suffix}/`)
+
+lst_length=${#inputDirs[@]}
+#lst_length=1
+
+
+for (( ind=1 ; ind <= lst_length ; ind++ ))
+	do
+        dir=${inputDirs[$((${ind} - 1))]}
+       ./launcher_SoftEllipses_Pointsets.sh $ind ${dir} ${continueFlag}  ${nthreads}
+	done
