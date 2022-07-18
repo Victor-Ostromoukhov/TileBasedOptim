35de5c4f70a122b3f0146cca2580cbdba0258c0b
Victor Ostromoukhov <ostrom@liristpd.univ-lyon1.fr>
2022-07-18 16:07:46 +0200

    vo

--- TileBasedOptim/TileBasedOptim.m
+++ TileBasedOptim/TileBasedOptim.m
@@ -2618,7 +2618,7 @@ gitpull
 math
 <<TileBasedOptim/TileBasedOptim.m
 
-Do[prepOptimDataPointsetsVarSize[mag, 6, i, False], {mag, 1, 4, .1}, {i, 4}]
+Do[prepOptimDataPointsetsVarSize[mag, 6, i, False], {mag, .7, 4, .1}, {i, 4}]
 
 
 *)
